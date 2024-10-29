7f63c53d924870b3667a3ba8de3441d0e415d466
Goutam Bhat <goutamdhogaleec@gmail.com>
2024-10-29 12:01:54 +0530

    second  commit

--- First.py
+++ First.py
@@ -1,9 +1,25 @@
 
-name=input("What is  your name ?")
+#01.Print 
 
-print("Hello\nbrother "+name)
+print("Hello Bhat")
 
-name="BHat"
+#02 Input 
 
-print("Hello\nbrother "+name)
+User_Name=input("What is your Name ?")
+
+print("My Name is " + User_Name)
+
+#03 Debugging --> To check Code step by step and  check out put for a line 
+
+#04 Errors --> Syntax error 
+
+
+#05 Variables is an name of the memory location which will store an values dynamically 
+
+# User Name --Not Valid 
+#01User  -- Not valid 
+#User_Name --> Valid 
+# follow Camle case for the better readbility 
+
+#06 Commment --> use for better readability 
 
